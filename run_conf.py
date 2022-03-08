f24e9cd7f1eee8b6a31d1b270302e7a4e25ba9ad
Panikos Christou <panikoschristou@yahoo.com>
2022-03-08 17:49:20 +0200

    Update run_conf.py

--- run_conf.py
+++ run_conf.py
@@ -25,7 +25,7 @@ def write_new_env(env: dict):
     for key, val in env.items():
         environ[key] = str(val)
         # Write changes to .env file.
-        dotenv.set_key(dotenv_file, key, val)
+        dotenv.set_key(dotenv_file, key, str(val))
 
 
 def update_env(key: str, val: str):
