b1c03c25a77b1b55ff3460619ca3e5aae9d77cca
Panikos Christou <panikoschristou@yahoo.com>
2022-04-13 17:08:24 +0300

    f

--- run_conf.py
+++ run_conf.py
@@ -124,7 +124,7 @@ def step_one(secs: int):
                         update_env('edge2_ml', model2)
 
                         curr_time = datetime.now().strftime('%H_%M_%d_%m')
-                        string = f'{pre},{bw},{model1},{model2},{curr_time}'
+                        string = f'{str1},{str2},{bw},{model1},{model2},{curr_time}'
                         write_to_done_file(string)
                         run_compose(secs)
 
