b5dfa04db8264469e696e927585b0bbce58da8f0
Panikos Christou <panikoschristou@yahoo.com>
2022-04-30 18:07:41 +0300

    Update download_models.py

--- base_image/download_models.py
+++ base_image/download_models.py
@@ -1,15 +1,12 @@
 # Use this code do invoke pytorch to download all the models we are using to this image so its not re downloaded on building child docker images
 
-# # fasterrcnn_mobilenet_v3_large_320_fpn, fasterrcnn_mobilenet_v3_large_fpn, fasterrcnn_resnet50_fpn, retinanet_resnet50_fpn, maskrcnn_resnet50_fpn
-
+# models = ["fasterrcnn_mobilenet_v3_large_320_fpn", "fasterrcnn_mobilenet_v3_large_fpn", "fasterrcnn_resnet50_fpn", "retinanet_resnet50_fpn", "maskrcnn_resnet50_fpn" , "fcos_resnet50_fpn" ,"keypointrcnn_resnet50_fpn","ssdlite320_mobilenet_v3_large" , "ssd300_vgg16" ]
 from torchvision import models
 
-fasterrcnn_resnet50_fpn = models.detection.fasterrcnn_resnet50_fpn(
-    pretrained=True)
-retinanet_resnet50_fpn = models.detection.retinanet_resnet50_fpn(
-    pretrained=True)
-fasterrcnn_mobilenet_v3_large_320_fpn = models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(
-    pretrained=True)
-fasterrcnn_mobilenet_v3_large_fpn = models.detection.fasterrcnn_mobilenet_v3_large_fpn(
-    pretrained=True)
-maskrcnn_resnet50_fpn = models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+model1 = models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+model1 = models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
+model1 = models.detection.retinanet_resnet50_fpn(pretrained=True)
+model1 = models.detection.fcos_resnet50_fpn(pretrained=True)
+model1 = models.detection.keypointrcnn_resnet50_fpn(pretrained=True)
+model1 = models.detection.ssdlite320_mobilenet_v3_large(pretrained=True)
+model1 = models.detection.ssd300_vgg16(pretrained=True)
